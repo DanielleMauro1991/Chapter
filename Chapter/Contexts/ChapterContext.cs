2d192f4a92d4247693111ddb33a1ae63e6f8a5e6
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-08-16 14:22:31 -0300

    Campos iniciais

---
+++ Chapter/Contexts/ChapterContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chapter.Contests
+{
+    public class ChapterContext : DbContext
+
+    {
+        public ChapterContext() { }
+        public ChapterContext (DbContextOptions < ChapterContext > options) : base(options) 
+        { }
+
+    }
+}
